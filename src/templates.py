f778947714ed23b4152cad6ce7d428e9bcf5e994
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-04-07 21:36:06 +0530

    fix relative path

--- src/templates.py
+++ src/templates.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from fastapi.templating import Jinja2Templates
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
